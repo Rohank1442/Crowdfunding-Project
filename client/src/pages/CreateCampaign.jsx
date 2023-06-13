28ea6672a98f518c0ba19b901d204dc71b465650
Rohank1442 <rohankumar1442@gmail.com>
2023-06-13 19:12:27 +0530

    C8

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,9 +1,135 @@
-import React from "react"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+
+import { useStateContext } from '../context';
+import { money } from "../assets";
+import { CustomButton, FormField } from "../components";
+import { checkIfImage } from "../utils";
 
 const CreateCampaign = () => {
-    return (
-        <div>CreateCampaign</div>
-    )
-} 
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isform, setIsform] = useState({
+    name: "",
+    title: "",
+    description: "",
+    target: "",
+    deadline: "",
+    image: "",
+  });
 
-export default CreateCampaign
+  const handleFormFieldChange = (fieldName, e) => {
+    setIsform({ ...isform, [fieldName]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    e.preventDefault();
+
+    console.log(form);
+  };
+
+  return (
+    <div
+      className="bg-[#1c1c24] flex justify-center items-center 
+        flex-col rounded-[10px] sm:p-10 p-4"
+    >
+      {isLoading && "Loader..."}
+      <div
+        className="flex justify-center items-center
+            p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]"
+      >
+        <h1
+          className="font-epilogue font-bold sm:text-[25px]
+                text-[18px] leading-[38px] text-white"
+        >
+          Start a Campaign
+        </h1>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="w-full mt-[65px]
+            flex flex-col gap-[30px]"
+      >
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField
+            LabelName="Your Name*"
+            placeholder="John Doe"
+            inputType="text"
+            value={isform.name}
+            handleChange={(e) => handleFormFieldChange("name", e)}
+          />
+          <FormField
+            LabelName="Campaign Title *"
+            placeholder="Write a Title"
+            inputType="text"
+            value={isform.title}
+            handleChange={(e) => handleFormFieldChange("title", e)}
+          />
+        </div>
+
+        <FormField
+          LabelName="Story *"
+          placeholder="Write your story"
+          isTextArea
+          value={isform.description}
+          handleChange={(e) => handleFormFieldChange("description", e)}
+        />
+        <div
+          className="w-full flex justify-start
+                items-center p-4 bg-[#8c6dfd] h-[120px]
+                rounded-[10px]"
+        >
+          <img
+            src={money}
+            alt="money"
+            className="w-[40px] h-[40px] object-contain"
+          />
+          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">
+            {" "}
+            You will get 100% of the raised amount
+          </h4>
+        </div>
+
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField
+            LabelName="Goal *"
+            placeholder="ETH 0.50"
+            inputType="text"
+            value={isform.target}
+            handleChange={(e) => handleFormFieldChange("target", e)}
+          />
+          <FormField
+            LabelName="End Date *"
+            placeholder="End Date"
+            inputType="date"
+            value={isform.deadline}
+            handleChange={(e) => handleFormFieldChange("deadline", e)}
+          />
+        </div>
+        <FormField
+          LabelName="Campaign image *"
+          placeholder="Place image URL of your 
+                        campaign"
+          inputType="url"
+          value={isform.image}
+          handleChange={(e) => handleFormFieldChange("image", e)}
+        />
+
+        <div
+          className="flex justify-center
+                    items-center mt-[40px]"
+        >
+          <CustomButton
+            btnType="submit"
+            title="Submit new campaign"
+            styles="bg-[#1dc071]"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateCampaign;
